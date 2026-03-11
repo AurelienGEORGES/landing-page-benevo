b133d6d7624dcb04fae3b2efaf6a57d7ab424c73
Aurélien Georges <aurelien.georges30@gmail.com>
2026-03-11 21:04:50 +0100

    feat: :zap: test img 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -71,7 +71,7 @@ export default function Hero() {
         }}>
           {/* iPhone Base */}
           <img
-            src="../../public/assets/LEAN-iPhone_Mockup_Hero_modifie.png"
+            src="top/Images/assets/LEAN-iPhone_Mockup_Hero_modifie.png"
             alt="iPhone BNEVO"
             style={{
               height: '100%',
