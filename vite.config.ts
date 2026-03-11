d618deff24f896282e815fca0305572f65203f22
Aurélien Georges <aurelien.georges30@gmail.com>
2026-03-11 16:04:28 +0100

    feat: :fire: ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-page-benevo/', // Ajoute cette ligne avec le nom de ton dépôt
+  base: '/landing-page-benevo/', // <--- CETTE LIGNE EST OBLIGATOIRE
 })
